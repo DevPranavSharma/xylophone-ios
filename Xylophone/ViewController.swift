cf60428165cfffc9966309c4acda79c5b51dbbda
Pranav Sharma <pranavs@geekyants.com>
2022-12-13 15:08:26 +0530

    add buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    func playSound(key:String){
+        do {
+            let pianoSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: key, ofType: "wav")!)
+            audioPlayer = try AVAudioPlayer(contentsOf: pianoSound as URL)
+                    audioPlayer.play()
+               } catch {
+                  // couldn't load file :(
+               }
+    }
+    @IBAction func onPressCButton(_ sender: UIButton) {
+        func setOpacityBack(){
+            sender.layer.opacity=1
+        }
+        print(sender.titleLabel?.text)
+        sender.layer.opacity=0.5;
+        playSound(key:(sender.titleLabel?.text)!)
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2, execute:setOpacityBack)
+            
+    }
+    
     
 
 }
